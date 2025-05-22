63114165c2210f37d2f61d6b3ebcae8e1bdb9f76
cristinaa45 <cristinavalarezo6@gmail.com>
2025-05-22 11:25:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 const cart = document.getElementById("cart");
 const addButtons = document.querySelectorAll("button[data-item]");
 const removeButton = document.getElementById("remove-btn");
+addButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    const itemName = button.getAttribute("data-item");
+
+    const option = document.createElement("option");
+    option.textContent = itemName;
+    option.value = itemName;
+
+    cart.appendChild(option);
+  });
+});
+removeButton.addEventListener("click", () => {
+  const selected = Array.from(cart.selectedOptions);  
+
+  selected.forEach(option => {
+    cart.removeChild(option);
+  });
+});
